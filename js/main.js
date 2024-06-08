b2dd69804cc4a70ced96848d3db2408e4d5337fe
MarVer <86689557+MvUltimate@users.noreply.github.com>
2024-06-08 15:35:02 +0200

    Update endGame

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ import { Duck } from "./Duck.js";
 import { Grass, Road, River } from "./Environment.js";
 import { drawScore, setScores, storedScore, getHighestScore, resetHighscore } from "./score.js";
 
+// Récupère le chemin de l'image sélectionnée depuis le localStorage
+const selectedImagePath = localStorage.getItem('selectedImagePath');
+
 // Canvas creation
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
@@ -43,8 +46,8 @@ document.addEventListener("keyup", function (event) {
 // Duck configuration and creation
 let duckSize = 40;
 let duckImage = new Image();
-duckImage.src = "./images/Duck.png";
-let duck = new Duck(duckSize, duckImage, 40, 280);
+duckImage.src = selectedImagePath || './images/Duck.png';
+const duck = new Duck(duckSize, duckImage, 40, 280);
 
 // Game loop
 reset();
